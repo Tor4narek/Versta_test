abd60bafac6c32e68f1bf28efb6c941cfea0a27d
User1 <User1@itmo.ru>
2026-04-27 14:29:06 +0300

    Add migrations

---
+++ Storage/Migrations/20260424175419_AddNewTables.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OrderNumber = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CitySender = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    AddressSender = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    CityReceiver = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    AddressReceiver = table.Column<string>(type: "character varying(300)", maxLength: 300, nullable: false),
+                    Weight = table.Column<decimal>(type: "numeric(10,3)", nullable: false),
+                    PickUpDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_OrderNumber",
+                table: "orders",
+                column: "OrderNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orders");
+        }
+    }
+}
